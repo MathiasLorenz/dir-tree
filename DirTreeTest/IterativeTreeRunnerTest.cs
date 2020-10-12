f633bb315dfb908ea9fb08efb59790070e481088
Mathias Lorenz <lorenz.mathias@gmail.com>
2020-10-12 23:15:29 +0200

    Added tests for various max depths.

--- DirTreeTest/IterativeTreeRunnerTest.cs
+++ DirTreeTest/IterativeTreeRunnerTest.cs
@@ -35,6 +35,30 @@ namespace DirTreeTest
             result.FileCount.ShouldBe(8);
         }
 
+        [TestMethod]
+        [DataRow(0, 0, 0)]
+        [DataRow(1, 2, 3)]
+        [DataRow(2, 3, 5)]
+        [DataRow(3, 3, 8)]
+        [DataRow(10, 3, 8)]
+        public void Run_MaxDepths_Runs(int maxDepth, int expectedDirectoryCount, int expectedFileCount)
+        {
+            string basePath = "test-dir";
+            SetUpFolders(basePath);
+
+            var options = new TreeRunnerOptions
+            {
+                Path = basePath,
+                MaxDepth = maxDepth
+            };
+            var iterativeRunner = new IterativeTreeRunner(options, 0);
+            var result = iterativeRunner.Run();
+
+            result.ShouldNotBeNull();
+            result.DirectoryCount.ShouldBe(expectedDirectoryCount);
+            result.FileCount.ShouldBe(expectedFileCount);
+        }
+
         private void SetUpFolders(string basePath)
         {
             var directoryInfo = new DirectoryInfo(basePath);
@@ -47,16 +71,16 @@ namespace DirTreeTest
             using (File.Create(newDirectory.FullName + @"\file1.txt")) { }
             using (File.Create(newDirectory.FullName + @"\file2")) { }
             using (File.Create(newDirectory.FullName + @"\file3.md")) { }
-
             newDirectory.CreateSubdirectory("sub-dir1");
             var subDirectory = newDirectory.CreateSubdirectory("sub-dir2");
+
             using (File.Create(subDirectory.FullName + @"\file4.log")) { }
             using (File.Create(subDirectory.FullName + @"\file5")) { }
-
             var subSubDirectory = subDirectory.CreateSubdirectory("sub");
-            using (File.Create(subDirectory.FullName + @"\f6")) { }
-            using (File.Create(subDirectory.FullName + @"\f7")) { }
-            using (File.Create(subDirectory.FullName + @"\f8")) { }
+
+            using (File.Create(subSubDirectory.FullName + @"\f6")) { }
+            using (File.Create(subSubDirectory.FullName + @"\f7")) { }
+            using (File.Create(subSubDirectory.FullName + @"\f8")) { }
         }
     }
 }
